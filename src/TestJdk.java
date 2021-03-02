3a08d64c09bc1cec4cee7d0dd00ba0f1ddd5b15e
jiaozq <2870203442@qq.com>
2021-03-02 10:05:26 +0800

    init jdk

---
+++ src/TestJdk.java
@@ -0,0 +1,8 @@
+import java.util.HashMap;
+
+public class TestJdk {
+    public static void main(String[] args) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 1);
+    }
+}
